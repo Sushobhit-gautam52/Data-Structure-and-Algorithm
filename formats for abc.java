878e909325ebfca54fdad9a0fba151d5bcf2b815
sushobhit_gautam <80403682+Sushobhit-gautam52@users.noreply.github.com>
2022-01-29 19:52:30 +0530

    Create formats for abc.java

---
+++ formats for abc.java
@@ -0,0 +1,21 @@
+public class abc_type {
+public static void ans(String inp, String ans)
+{
+  // base condition
+
+  if(inp.length() ==0 )
+  {
+    System.out.println(ans);
+    return;
+  }
+  for(int i =0;i<inp.length();i++)
+  {
+      String remainingString= inp.substring(0,i)+ inp.substring(i+1) ;
+      char extract = inp.charAt(i);
+      ans(remainingString, ans +extract);
+  }
+}
+  public static void main(String[] args) {
+    ans("abc"," ");
+  }
+}
