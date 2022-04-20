b93a6c7cbea97828833a1615fd66b539b9f8adda
sushobhit_gautam <80403682+Sushobhit-gautam52@users.noreply.github.com>
2022-04-20 19:58:07 +0530

    Create count of nodes in Tree.java

---
+++ count of nodes in Tree.java
@@ -0,0 +1,46 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class myclas{
+    static class Node{
+        int data;
+        Node left,right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+     static class binarytree{
+        static int index=-1;
+        public Node build(int nodes[]){
+            index++;
+            if(nodes[index]==-1){
+                return null;
+            }
+            Node newnode=new Node(nodes[index]);
+            newnode.left=build(nodes);
+            newnode.right=build(nodes);
+            return newnode;
+
+        }
+
+    }
+    public static int countnode(Node root){
+        if(root==null){
+            return 0;
+        }
+        int leftnodes=countnode(root.left);
+        int rightnodes=countnode(root.right);
+        return leftnodes + rightnodes +1;
+    }
+
+    public static void main(String[] args) {
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        binarytree tree=new binarytree();
+        Node root=tree.build(nodes);
+
+      
+        System.out.println(countnode(root));
+    }
+}
